af528c1a71ffa9f038e68182c6baaffa2263cb6f
Ruekami <michael.b.simard@gmail.com>
2012-11-07 09:03:11 -0500

    - deploy fix
    - unitCreation: select -> menu

--- ForgottenSchism/ForgottenSchism/screen/UnitCreation.cs
+++ ForgottenSchism/ForgottenSchism/screen/UnitCreation.cs
@@ -16,40 +16,57 @@ namespace ForgottenSchism.screen
     public class UnitCreation : Screen
     {
         Label lbl_unitCre;
+        Label lbl_chooseLdr;
 
         TextBox txt_unitName;
 
-        Select sel_leader;
+        Menu menu_leader;
 
+        Link lnk_choose;
         Link lnk_create;
 
         Army army;
 
+        Boolean choosing;
+
         public UnitCreation(Army a)
         {
+            choosing = false;
+
             army = a;
 
             lbl_unitCre = new Label("Unit Creation");
             lbl_unitCre.Color = Color.Gold;
             lbl_unitCre.Position = new Vector2(50, 30);
 
+            lbl_chooseLdr = new Label("Available Characters");
+            lbl_chooseLdr.Color = Color.Gold;
+            lbl_chooseLdr.Position = new Vector2(400, 90);
+
             txt_unitName = new TextBox(10);
             txt_unitName.Position = new Vector2(90, 90);
 
-            sel_leader = new Select();
-            sel_leader.Position = new Vector2(90, 150);
+            menu_leader = new Menu(10);
+            menu_leader.Position = new Vector2(400, 90);
             foreach(Character c in army.Standby)
             {
-                sel_leader.add(c.Name);
+                menu_leader.add(new Link(c.Name));
             }
+            menu_leader.TabStop = false;
 
             lnk_create = new Link("Create Unit");
             lnk_create.Position = new Vector2(90, 210);
             lnk_create.selected = create;
 
+            lnk_choose = new Link("Choose Leader");
+            lnk_choose.Position = new Vector2(90, 150);
+            lnk_choose.selected = choose;
+
             cm.add(lbl_unitCre);
+            cm.add(lbl_chooseLdr);
             cm.add(txt_unitName);
-            cm.add(sel_leader);
+            cm.add(menu_leader);
+            cm.add(lnk_choose);
             cm.add(lnk_create);
         }
 
@@ -59,15 +76,42 @@ namespace ForgottenSchism.screen
 
             if (InputHandler.keyReleased(Keys.Escape))
             {
-                StateManager.Instance.goBack();
+                if (choosing)
+                {
+                    choosing = false;
+                    menu_leader.TabStop = false;
+                    txt_unitName.TabStop = true;
+                    lnk_choose.TabStop = true;
+                    lnk_create.TabStop = true;
+
+                    menu_leader.HasFocus = false;
+                    lnk_choose.HasFocus = true;
+                }
+                else
+                {
+                    StateManager.Instance.goBack();
+                }
             }
         }
 
         private void create(object sender, EventArgs e)
         {
-            army.Units.Add(new Unit(army.Standby[sel_leader.Selection], txt_unitName.Text));
-            army.Standby.RemoveAt(sel_leader.Selection);
+            army.Units.Add(new Unit(army.Standby[menu_leader.Selected], txt_unitName.Text));
+            army.Standby.RemoveAt(menu_leader.Selected);
             StateManager.Instance.changeCur(new UnitManage(army, army.Units.Count - 1));
+        }
+
+        private void choose(object sender, EventArgs e)
+        {
+            choosing = true;
+
+            menu_leader.TabStop = true;
+            txt_unitName.TabStop = false;
+            lnk_choose.TabStop = false;
+            lnk_create.TabStop = false;
+
+            lnk_choose.HasFocus = false;
+            menu_leader.HasFocus = true;
         }
     }
 }
