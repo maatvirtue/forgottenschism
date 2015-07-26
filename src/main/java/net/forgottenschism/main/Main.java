1ecc3064e17fb4fe48199f9ff3e10eca75f064f3
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2015-07-26 19:07:38 -0400

    - added logback configuration file - improved game startup and shutdown

--- src/main/java/net/forgottenschism/main/Main.java
+++ src/main/java/net/forgottenschism/main/Main.java
@@ -1,25 +1,50 @@
 package net.forgottenschism.main;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 
 import net.forgottenschism.constants.Constants;
 import org.newdawn.slick.CanvasGameContainer;
 import org.newdawn.slick.SlickException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 
 public class Main
 {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) throws SlickException
     {
 		loadNativeLibs();
 
-        CanvasGameContainer app = new CanvasGameContainer(new ForgottenSchismGame());
+        final CanvasGameContainer app = new CanvasGameContainer(new ForgottenSchismGame());
         app.getContainer().setAlwaysRender(true);
 
         JFrame frame = new JFrame();
         frame.setSize(Constants.DEFAULT_GAME_SIZE.getWidth(), Constants.DEFAULT_GAME_SIZE.getHeight());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.getContentPane().add(app);
+
+        frame.addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                app.setEnabled(false);
+                app.getContainer().exit();
+                app.dispose();
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e)
+            {
+                System.exit(0);
+            }
+        });
 
         frame.setVisible(true);
         app.start();
@@ -27,6 +52,8 @@ public class Main
 	
 	private static void loadNativeLibs()
 	{
-		System.setProperty("org.lwjgl.librarypath", new File("native-libs").getAbsolutePath());
+        String nativeLibraryPath = System.getProperty("java.library.path");
+
+		System.setProperty("org.lwjgl.librarypath", new File(nativeLibraryPath).getAbsolutePath());
 	}
 }
