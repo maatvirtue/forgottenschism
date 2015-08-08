538810a3f92bcacbabfcf70e30fdb33b6aece414
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2015-08-08 16:33:48 -0400

    added GeneritUtil.maximum() and GeneritUtil.sum()

--- src/main/java/net/forgottenschism/util/GenericUtil.java
+++ src/main/java/net/forgottenschism/util/GenericUtil.java
@@ -6,4 +6,19 @@ public class GenericUtil
 	{
 		return a<b ? a : b;
 	}
+
+	public static int maximum(int a, int b)
+	{
+		return a>b ? a : b;
+	}
+
+	public static int sum(int[] numbers)
+	{
+		int sum = 0;
+
+		for(int i = 0; i<numbers.length; i++)
+			sum += numbers[i];
+
+		return sum;
+	}
 }
