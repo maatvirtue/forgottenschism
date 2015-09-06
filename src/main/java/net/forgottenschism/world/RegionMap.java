a16f666d6267685ba51a97a45fb884931c8dbe1d
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2015-09-06 19:04:04 -0400

    added more options to RegionMap constructor

--- src/main/java/net/forgottenschism/world/RegionMap.java
+++ src/main/java/net/forgottenschism/world/RegionMap.java
@@ -3,10 +3,13 @@ package net.forgottenschism.world;
 import net.forgottenschism.gui.bean.Size2d;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class RegionMap
 {
-	private java.util.Map<Coordinate, Tile> tileMap;
+	private static final Terrain DEFAULT_TERRAIN = Terrain.BLUE;
+
+	private Map<Coordinate, Tile> tileMap;
 	private Size2d size;
 
 	public RegionMap()
@@ -16,12 +19,17 @@ public class RegionMap
 
 	public RegionMap(int width, int height)
 	{
+		this(50, 50, DEFAULT_TERRAIN);
+	}
+
+	public RegionMap(int width, int height, Terrain terrain)
+	{
 		size = new Size2d(width, height);
 		tileMap = new HashMap<>(width*height);
 
 		for(int y = 0; y<height; y++)
 			for(int x = 0; x<width; x++)
-				tileMap.put(new Coordinate(x, y), new Tile(Terrain.BLUE));
+				tileMap.put(new Coordinate(x, y), new Tile(terrain));
 	}
 
 	public Tile getTile(Coordinate coordinate)
