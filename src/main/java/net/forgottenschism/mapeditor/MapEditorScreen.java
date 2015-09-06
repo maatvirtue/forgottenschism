53d6ba71ddd12c58fbb5e9c5bb38e5ecbda17a64
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2015-09-06 19:05:14 -0400

    added controls list and new map menu to map editor screen.

--- src/main/java/net/forgottenschism/mapeditor/MapEditorScreen.java
+++ src/main/java/net/forgottenschism/mapeditor/MapEditorScreen.java
@@ -1,26 +1,25 @@
 package net.forgottenschism.mapeditor;
 
+import net.forgottenschism.gui.Window;
 import net.forgottenschism.gui.bean.GraphicMeasure;
 import net.forgottenschism.gui.bean.GraphicalUnit;
 import net.forgottenschism.gui.bean.Orientation2d;
-import net.forgottenschism.gui.control.Label;
-import net.forgottenschism.gui.control.MapControl;
-import net.forgottenschism.gui.control.Picture;
-import net.forgottenschism.gui.control.Spinner;
+import net.forgottenschism.gui.bean.Size2d;
+import net.forgottenschism.gui.control.*;
 import net.forgottenschism.gui.event.KeyEvent;
 import net.forgottenschism.gui.impl.AbstractScreen;
-import net.forgottenschism.gui.layout.LinearLayout;
-import net.forgottenschism.gui.layout.LinearLayoutParameters;
-import net.forgottenschism.gui.layout.RelativeLayout;
-import net.forgottenschism.gui.layout.RelativeLayoutParameters;
+import net.forgottenschism.gui.impl.WindowImpl;
+import net.forgottenschism.gui.layout.*;
 import net.forgottenschism.gui.theme.ColorTheme;
 import net.forgottenschism.gui.theme.ColorThemeElement;
 import net.forgottenschism.gui.theme.Theme;
-import net.forgottenschism.world.Map;
+import net.forgottenschism.world.RegionMap;
 import net.forgottenschism.world.Terrain;
 import net.forgottenschism.world.Tile;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Input;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,13 +31,18 @@ public class MapEditorScreen extends AbstractScreen
 	private static Logger logger = LoggerFactory.getLogger(MapEditorScreen.class);
 	private static ColorTheme DEFAULT_COLOR_THEME = Theme.getDefaultTheme().getColorTheme();
 	private static Terrain DEFAULT_SELECTED_TERRAIN = Terrain.RED;
+	private static Terrain DEFAULT_NEWMAP_TERRAIN = Terrain.BLUE;
 
-	private Map map;
+	private RegionMap regionMap;
 	private File mapFile;
 
+	private MapControl mapControl;
 	private Picture selectedTerrainPicture;
 	private Spinner<Terrain> terrainSpinner;
 	private Label mapFileLabel;
+	private Window newMapWindow;
+	private Textbox newMapWidth;
+	private Textbox newMapHeight;
 
 	public MapEditorScreen()
 	{
@@ -49,35 +53,137 @@ public class MapEditorScreen extends AbstractScreen
 
 	private void loadDefaultMap()
 	{
-		map = new Map(50, 50);
+		regionMap = new RegionMap(50, 50);
 	}
 
 	private void setupGui()
 	{
 		setupMainWindow();
+		setupNewMapWindow();
 	}
 
 	private void setupMainWindow()
 	{
 		getMainWindow().setLayout(new LinearLayout(Orientation2d.HORIZONTAL, 0));
 
-		MapControl mapControl = new MapControl(map);
+		mapControl = new MapControl(regionMap);
 		LinearLayoutParameters mapPosition = new LinearLayoutParameters();
 		mapPosition.setWeight(1f);
 		mapControl.setLayoutParameters(mapPosition);
 		mapControl.setSelectionListener(coordinate ->
 		{
-			map.getTile(coordinate).setTerrain(terrainSpinner.getSelection());
+			regionMap.getTile(coordinate).setTerrain(terrainSpinner.getSelection());
 		});
 		addControl(mapControl);
 
 		RelativeLayout sideMenu = new RelativeLayout();
 		LinearLayoutParameters panelPosition = new LinearLayoutParameters();
-		panelPosition.setLength(300);
+		panelPosition.setLength(400);
 		sideMenu.setLayoutParameters(panelPosition);
 		addControl(sideMenu);
 
 		setupSideMenu(sideMenu);
+	}
+
+	private void setupNewMapWindow()
+	{
+		newMapWindow = new WindowImpl(this);
+		newMapWindow.setBackgroundColor(Color.gray);
+		newMapWindow.setLayout(new RelativeLayout());
+
+		Label newMapTitle = new Label("New Map");
+		RelativeLayoutParameters topCenter = new RelativeLayoutParameters();
+		topCenter.horizontallyCentered();
+		topCenter.setTopPosition(10, GraphicalUnit.PIXEL);
+		newMapTitle.setLayoutParameters(topCenter);
+		newMapTitle.setTextColor(DEFAULT_COLOR_THEME.getColor(ColorThemeElement.LABEL_TITLE));
+		newMapWindow.addControl(newMapTitle);
+
+		TableLayout centerPanel = new TableLayout();
+		RelativeLayoutParameters center = new RelativeLayoutParameters();
+		center.horizontallyCentered();
+		center.verticalyCentered();
+		centerPanel.setLayoutParameters(center);
+		newMapWindow.addControl(centerPanel);
+
+		setupNewMapWindowCenterPanel(centerPanel);
+
+		Link applyNewMap = new Link("Apply new map");
+		RelativeLayoutParameters bottomCenter = new RelativeLayoutParameters();
+		bottomCenter.horizontallyCentered();
+		bottomCenter.setBottomPosition(10, GraphicalUnit.PIXEL);
+		applyNewMap.setLayoutParameters(bottomCenter);
+		applyNewMap.setSelectionListener(control ->
+		{
+			Size2d newMapSize = getNewMapSizeFromGui();
+
+			applyNewMap(newMapSize, DEFAULT_NEWMAP_TERRAIN);
+		});
+		newMapWindow.addControl(applyNewMap);
+	}
+
+	private void setupNewMapWindowCenterPanel(TableLayout centerPanel)
+	{
+		RelativeLayoutParameters centered = new RelativeLayoutParameters();
+		centered.horizontallyCentered();
+		centered.verticalyCentered();
+
+		RowLayout widthRow = new RowLayout();
+		centerPanel.addControl(widthRow);
+
+		Label width = new Label("Width");
+		width.setLayoutParameters(centered);
+		widthRow.addControl(width);
+
+		newMapWidth = new Textbox(2);
+		newMapWidth.setLayoutParameters(centered);
+		widthRow.addControl(newMapWidth);
+
+		RowLayout heightRow = new RowLayout();
+		centerPanel.addControl(heightRow);
+
+		Label height = new Label("Height");
+		height.setLayoutParameters(centered);
+		heightRow.addControl(height);
+
+		newMapHeight = new Textbox(2);
+		newMapHeight.setLayoutParameters(centered);
+		heightRow.addControl(newMapHeight);
+	}
+
+	private Size2d getNewMapSizeFromGui()
+	{
+		int width;
+		int height;
+
+		try
+		{
+			width = Integer.parseInt(newMapWidth.getText());
+		}
+		catch(NumberFormatException e)
+		{
+			width = 10;
+		}
+
+		try
+		{
+			height = Integer.parseInt(newMapHeight.getText());
+		}
+		catch(NumberFormatException e)
+		{
+			height = 10;
+		}
+
+		return new Size2d(width, height);
+	}
+
+	private void applyNewMap(Size2d newMapSize, Terrain terrain)
+	{
+		regionMap = new RegionMap(newMapSize.getWidth(), newMapSize.getHeight(), terrain);
+
+		mapControl.setRegionMap(regionMap);
+
+		newMapWindow.close();
 	}
 
 	private void setupSideMenu(RelativeLayout sideMenu)
@@ -91,6 +197,7 @@ public class MapEditorScreen extends AbstractScreen
 
 		setupTerrainMenu(centerSideMenuPanel);
 		setupMapFileMenu(centerSideMenuPanel);
+		setupControlsMenu(centerSideMenuPanel);
 	}
 
 	private void setupMapFileMenu(LinearLayout centerSideMenuPanel)
@@ -101,6 +208,28 @@ public class MapEditorScreen extends AbstractScreen
 
 		mapFileLabel = new Label("None");
 		centerSideMenuPanel.addControl(mapFileLabel);
+	}
+
+	private void setupControlsMenu(LinearLayout centerSideMenuPanel)
+	{
+		Label controlsLabel = new Label("Controls");
+		controlsLabel.setTextColor(DEFAULT_COLOR_THEME.getColor(ColorThemeElement.LABEL_TITLE));
+		centerSideMenuPanel.addControl(controlsLabel);
+
+		Label moveSelectionLabel = new Label("ARROWS Move selection");
+		centerSideMenuPanel.addControl(moveSelectionLabel);
+
+		Label changeTerrainLabel = new Label("LSHIFT/RSHIFT Change terrain");
+		centerSideMenuPanel.addControl(changeTerrainLabel);
+
+		Label applyTerrainLabel = new Label("SPACE Apply terrain");
+		centerSideMenuPanel.addControl(applyTerrainLabel);
+
+		Label openLoadSaveMenuLabel = new Label("ENTER Open load/save menu");
+		centerSideMenuPanel.addControl(openLoadSaveMenuLabel);
+
+		Label newTerrainMenuLabel = new Label("N New map");
+		centerSideMenuPanel.addControl(newTerrainMenuLabel);
 	}
 
 	private void setupTerrainMenu(LinearLayout centerSideMenuPanel)
@@ -128,18 +257,27 @@ public class MapEditorScreen extends AbstractScreen
 	@Override
 	public boolean screenKeyReleased(KeyEvent keyEvent)
 	{
-		if(keyEvent.getKeyCode()==Input.KEY_LSHIFT)
+		if(getMainWindow().hasFocus())
 		{
-			terrainSpinner.selectPreviousOption();
-			return false;
+
+			if(keyEvent.getKeyCode()==Input.KEY_LSHIFT)
+			{
+				terrainSpinner.selectPreviousOption();
+				return false;
+			}
+			else if(keyEvent.getKeyCode()==Input.KEY_RSHIFT)
+			{
+				terrainSpinner.selectNextOption();
+				return false;
+			}
+			else if(keyEvent.getKeyCode()==Input.KEY_N)
+			{
+				newMapWindow.show();
+				return false;
+			}
+			else if(keyEvent.getKeyCode()==Input.KEY_TAB)
+				return false;
 		}
-		else if(keyEvent.getKeyCode()==Input.KEY_RSHIFT)
-		{
-			terrainSpinner.selectNextOption();
-			return false;
-		}
-		if(keyEvent.getKeyCode()==Input.KEY_TAB)
-			return false;
 
 		return true;
 	}
